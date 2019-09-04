2b3c2e3edcc432bd708c5be12f93625c72f55818
Kayode Olaniyi <kaysolaniyi@gmail.com>
2019-09-04 10:54:49 +0100

    added models

--- book/models.py
+++ book/models.py
@@ -1,4 +1,18 @@
 from django.db import models
 
+
 # Create your models here.
 
+
+class Book(models.Model):
+    name = models.CharField(max_length=150, null=False)
+    isbn = models.CharField(max_length=20, null=False)
+    country = models.CharField(max_length=100, null=False)
+    number_of_pages = models.IntegerField(null=False)
+    publisher = models.IntegerField(null=False)
+    release_date = models.DateTimeField(null=False)
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=150)
+    book = models.CharField(Book)
